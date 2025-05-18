1991cdba881b48bea58195b6117db08230defdd4
Allan Brim <allanbrim@Allans-MacBook-Air.local>
2025-05-18 23:09:01 +0200

    opnings houes

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
             ></Image>
             <div className=" object-cover absolute top-0 left-0 w-full h-full bg-primary opacity-90"></div>
             <div className="absolute top-0 left-0 flex items-end w-full h-full">
-                <div className="w-full h-auto sticky bottom-96 rounded-xl flex items-center justify-between flex-wrap z-10">
+                <div className="w-full h-auto sticky bottom-96 rounded-xl flex items-center justify-around flex-wrap z-10">
                     {/* <Image
                         alt="Hero Image"
                         src="/images/logo.svg"
@@ -34,10 +34,12 @@ const Hero = () => {
                         Mood Coffee
                     </h1>
                     <div className="w-auto text-justify">
-                        <p className="text-lg text-bg">
+                        <p className="text-sm text-bg">
                             <strong>Our oping hours are</strong>
-                            <br /> 8 AM to 8 PM, Monday to Friday
-                            <br /> 9 AM to 9 PM on weekends
+                            <br /> 7 AM to 8 PM, Monday to thursday
+                            <br /> 7 AM to 9 PM Friday
+                            <br /> 9 AM to 10 PM Saturday
+                            <br /> 9 AM to 8 PM Sunday
                         </p>
                     </div>
                 </div>
